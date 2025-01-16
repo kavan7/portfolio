2ab2748da9b1e80656371babf9dc140e78099d3b
kavan7 <abeyratnekavan@gmail.com>
2025-01-15 19:05:26 -0500

    portfolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,23 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Kavan Abeyratne | Portfolio",
-  description: "My online portfolio",
+  description:
+    "A driven and innovative student with a passion for creating meaningful impact through leadership and community engagement.",
+  openGraph: {
+    type: "website",
+    title: "Raj Gill Law Corporation - Trusted Family Lawyers",
+    description:
+      "Expert legal services in Surrey & Abbotsford, specializing in family law, estate planning, and more. Schedule a free consultation today with Raj Gill Law Corporation.",
+    images: "https://rajgilllaw.com/assets/preview-image.jpg",
+    url: "https://rajgilllaw.com",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Raj Gill Law Corporation - Trusted Family Lawyers",
+    description:
+      "Expert legal services in Surrey & Abbotsford, specializing in family law, estate planning, and more. Schedule a free consultation today with Raj Gill Law Corporation.",
+    images: "https://rajgilllaw.com/assets/preview-image.jpg",
+  },
 };
 
 export default function RootLayout({
